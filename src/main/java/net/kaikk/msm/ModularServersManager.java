6c2a1c480be8d913048327d15dfc99f00d751e56
Kai <kai.kikuchi27@gmail.com>
2017-06-20 09:12:08 +0200

    Version 0.9.6

--- src/main/java/net/kaikk/msm/ModularServersManager.java
+++ src/main/java/net/kaikk/msm/ModularServersManager.java
@@ -41,7 +41,7 @@ import net.kaikk.msm.server.Server;
 import net.kaikk.msm.util.Utils;
 
 public class ModularServersManager {
-	public static final String VERSION = "0.9.4";
+	public static final String VERSION = "0.9.6";
 	
 	protected static ModularServersManager instance;
 	protected Config config;
