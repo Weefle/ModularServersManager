3dbf47f27f1e11f32601a71ba354021c4d603088
Kai <kai.kikuchi27@gmail.com>
2017-09-03 10:33:10 +0200

    Version 0.9.11

--- src/main/java/net/kaikk/msm/AppInfo.java
+++ src/main/java/net/kaikk/msm/AppInfo.java
@@ -1,6 +1,6 @@
 package net.kaikk.msm;
 
 public class AppInfo {
-	public static final int BUILD = 10;
-	public static final String VERSION = "0.9.10";
+	public static final int BUILD = 11;
+	public static final String VERSION = "0.9.11";
 }
